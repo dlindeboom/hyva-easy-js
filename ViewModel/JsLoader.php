9d6a8de21ba42365e003e4f5241b54f88e3a2cbc
dlindeboom <dennis@mooore.nl>
2022-05-13 15:33:33 +0200

    WIP

--- ViewModel/JsLoader.php
+++ ViewModel/JsLoader.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hyva\EasyJs\ViewModel;
 
+use Magento\Framework\App\CacheInterface;
 use Magento\Framework\Filesystem\Driver\File;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Framework\View\Element\Template\File\Resolver;
@@ -12,11 +13,13 @@ class JsLoader implements ArgumentInterface
 {
     private File $file;
     private Resolver $resolver;
+    private CacheInterface $cache;
 
-    public function __construct(File $file, Resolver $resolver)
+    public function __construct(File $file, Resolver $resolver, CacheInterface $cache)
     {
         $this->file = $file;
         $this->resolver = $resolver;
+        $this->cache = $cache;
     }
 
     public function require(string $jsFilePath): string
@@ -25,5 +28,28 @@ class JsLoader implements ArgumentInterface
         $content = $this->file->fileGetContents($path);
 
         return sprintf("<script>%s</script>", $content);
+    }
+
+    public function requireOnce(string $jsFilePath): string
+    {
+        $cacheCode = $this->makeUniqueString($jsFilePath);
+        $content = $this->cache->load(sprintf('js_load_%s', $this->makeUniqueString($jsFilePath)));
+
+        if (empty($content)) {
+            $path = $this->resolver->getTemplateFileName($jsFilePath);
+            $content = $this->file->fileGetContents($path);
+
+            $this->cache->save(sprintf('js_load_%s', $content));
+        }
+
+        $path = $this->resolver->getTemplateFileName($jsFilePath);
+        $content = $this->file->fileGetContents($path);
+
+        return sprintf("<script>%s</script>", $content);
+    }
+
+    private function makeUniqueString($jsFilePath)
+    {
+        return bin2hex($jsFilePath);
     }
 }
